1769dbb9625837087e13a659e5b2c690cceea8d7
Developers <admin@vertin-go.com>
2019-01-10 10:18:36 +0100

    Update AdminController.php

--- src/AppBundle/Controller/Admin/AdminController.php
+++ src/AppBundle/Controller/Admin/AdminController.php
@@ -55,7 +55,7 @@ class AdminController extends BaseAdminController
         return $this->csvExporter->getResponseFromQueryBuilder(
             $queryBuilder,
             YouTubeAnalytics::class,
-            'genuses.csv'
+            'YouTubeAnalytics.csv'
         );
     }
 
